c203f6149abe183c7dc6322870cfe52af9a06c6c
toasted-cornkernels <volcano0909@gmail.com>
2026-06-12 22:54:01 -0400

    project structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstdlib>
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello " << getenv("USER") <<"! This is the Monkey programming language!" << "\n";
+  std::cout << "Feel free to type in commands \n";
+
+	// repl.start(std::cin, std::cout);
   return 0;
 }
